bced47ad8b250af76de8164a9ea4a518cc6ef3f9
JishnuVijayan <108149185+JishnuVijayan@users.noreply.github.com>
2024-12-04 21:43:39 +0530

    Datamodel class
    Individual Datamodel class for wishes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WishItem } from '../shared/wishlist/wishListItem';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
+  wishList = [
+    new WishItem('Secure a Internship', true),
+    new WishItem('Complete Engineering without backlogs', false),
+    new WishItem('Secure a good job'),
+  ];
   title = 'wishlist';
 }
