eb27a1f566d524b95219560eafd18d2a2fc2d449
JishnuVijayan <108149185+JishnuVijayan@users.noreply.github.com>
2024-12-05 21:19:11 +0530

    Input for adding wishes
    Input for adding wishes which restrict empty wishes and force clears the input field.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,23 +3,43 @@ import { Component } from '@angular/core';
 import { WishItem } from '../shared/wishlist/wishListItem';
 import { CommonModule } from '@angular/common';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatLabel } from '@angular/material/form-field';
+import { MatFormField } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, MatCheckboxModule],
+  imports: [
+    CommonModule,
+    MatCheckboxModule,
+    MatLabel,
+    MatFormField,
+    FormsModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   standalone: true,
 })
 export class AppComponent {
   wishList: WishItem[] = [
-    new WishItem('Secure a Internship', true),
-    new WishItem('Complete Engineering without backlogs', false),
-    new WishItem('Secure a good job'),
+    // new WishItem('Secure a Internship', true),
+    // new WishItem('Complete Engineering without backlogs', false),
+    // new WishItem('Secure a good job'),
   ];
   title = 'Wishlist';
   checkBox(item: WishItem) {
     item.isComplete = !item.isComplete;
     console.log(item);
+  }
+
+  newWishText = '';
+  addNewWish() {
+    if (this.newWishText.trim()) {
+      this.wishList.push(new WishItem(this.newWishText));
+      this.newWishText = '';
+      document.querySelector<HTMLInputElement>(
+        'input[name="new-wish-text"]'
+      )!.value = '';
+    }
   }
 }
